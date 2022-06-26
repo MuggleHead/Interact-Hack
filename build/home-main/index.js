c59353334f1c4ef0c6039915c3edea5e19ce1831
Akshay A Kalekar <50509605+akshay-Kalekar@users.noreply.github.com>
2022-06-26 16:34:44 +0530

    Add files via upload

---
+++ build/home-main/index.js
@@ -0,0 +1,23 @@
+(function($) {
+  $(function() {
+
+    $('#navbar-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+
+    $('#navbar-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+        $('html').click(function() {
+          $('.navbar-dropdown').hide();
+        });
+      });
+})(jQuery);
+let root = document.documentElement;
+const section1 = document.querySelector('.header')
+
+window.addEventListener('scroll', () => {
+  const y = 1 + (window.scrollY || window.pageYOffset);
+   root.style.setProperty('--gradient-percent', y * 4 + "px");
+  console.log(y)
+})
